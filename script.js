6e19cadeeed2f5b1667eccd4a9b6a9046f487a17
Antarpreet <pnarain82@gmail.com>
2023-07-25 11:07:30 +1000

    complete

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-document.getElementById("sub_btn").addEventListener("click", function (e) {
+const emailElm = document.getElementById("email_field");
+document.getElementById("sign_upForm").addEventListener("submit", function (e) {
   e.preventDefault();
-  var email = document.getElementById("email_field").value;
-
-  if (email.length === 0) {
-    console.log("enter something");
+  resetErrorMsg();
+  var email = emailElm.value;
+  var regexp1 = /^[a-zA-Z0-9_]+@[a-zA-Z0-9-_]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (email.match(regexp1)) {
+    displaySucess(email);
+    document.getElementById("m").style.display = "none";
+    var elm = document.getElementById("success");
+    elm.classList.add("show");
   } else {
-    if (email.includes("@") && email.includes(".com")) {
-      console.log(email);
-    } else {
-      console.log("no valid email");
-    }
+    errorMsg();
   }
 });
+const resetErrorMsg = () => {
+  document.getElementById("error_email").innerHTML = "";
+  emailElm.style.color = "black";
+  emailElm.style.border = "1px solid black";
+  emailElm.style.backgroundColor = " white";
+};
+const errorMsg = () => {
+  document.getElementById("error_email").innerHTML = "Valid email required!";
+  emailElm.style.color = "red";
+  emailElm.style.border = "1px solid red";
+  emailElm.style.backgroundColor = " rgb(253, 157, 157)";
+};
+const displaySucess = (email) => {
+  document.getElementById("display_email").innerHTML = email;
+};
